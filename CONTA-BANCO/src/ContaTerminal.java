13d8d0de20d677c27922716a2edd3951933b0507
bruno <brunopaiva98@hotmail.com>
2025-07-21 21:02:43 -0300

    codigo criado

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+import conta.conta;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem-vindo ao Banco Java!");
+        conta minhaConta = new conta();
+
+        System.out.println("Por favor, digite número da conta:");
+        minhaConta.setNumeroConta(sc.nextLine());
+        System.out.println("Por favor, digite o nome do cliente:");
+        minhaConta.setNomeCliente(sc.nextLine());
+        System.out.println("Por favor, digite o número da agência:");
+        minhaConta.setAgencia(sc.nextLine());
+        System.out.println("Por favor, digite o saldo da conta:");
+        minhaConta.setSaldo(sc.nextDouble());
+
+
+        System.out.println("==============Banco Java=============");
+        System.out.println("Olá, " + minhaConta.getNomeCliente() + 
+                           "! Obrigado por criar uma conta em nosso banco, " 
+                           + "sua agência é " + minhaConta.getAgencia() +
+                            ", Conta " + minhaConta.getNumeroConta() + ", e seu saldo é " + minhaConta.getSaldo() + " já está disponível." );
+        sc.close();
     }
+    
 }
---
+++ CONTA-BANCO/src/conta/conta.java
@@ -0,0 +1,34 @@
+package conta;
+
+public class conta {
+    private String nomeCliente;
+    private String numeroConta; 
+    private String agencia;
+    private double saldo;
+    
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}   
